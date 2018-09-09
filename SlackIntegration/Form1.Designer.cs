7f06cbff594a1cd798514a26c036bf00e9159a94
Mesut KAYA <mesutkaya@anadolu.edu.tr>
2018-09-09 08:15:52 +0300

    Add project files.

---
+++ SlackIntegration/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SlackIntegration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSlack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSlack
+            // 
+            this.btnSlack.Location = new System.Drawing.Point(206, 96);
+            this.btnSlack.Name = "btnSlack";
+            this.btnSlack.Size = new System.Drawing.Size(235, 61);
+            this.btnSlack.TabIndex = 0;
+            this.btnSlack.Text = "Send Message To Slack";
+            this.btnSlack.UseVisualStyleBackColor = true;
+            this.btnSlack.Click += new System.EventHandler(this.btnSlack_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSlack);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSlack;
+    }
+}
+
---
+++ SlackIntegration/Form1.cs
@@ -0,0 +1,34 @@
+﻿using Slack.Webhooks;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SlackIntegration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSlack_Click(object sender, EventArgs e)
+        {
+            string urlWithAccessToken = "https://hooks.slack.com/services/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";
+            SlackClient slackClient = new SlackClient(urlWithAccessToken);
+            var slackMessage = new SlackMessage
+            {
+                Text = "yazılım Test Mesajı!",
+                IconEmoji = Emoji.Angry,
+                Username = "Mesut Kaya"
+            };
+            slackClient.Post(slackMessage);
+        }
+    }
+}
